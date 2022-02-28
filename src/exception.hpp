b47ba37ba707dbcfb418bb18fa2950a186fd729a
rainboy <rainboylvx@qq.com>
2022-02-28 15:17:53 +0800

    [加入] 提供了反射的能力

--- src/exception.hpp
+++ src/exception.hpp
@@ -2,5 +2,15 @@
 
 namespace cppjson {
     
+struct NoSuchClassException:public std::exception
+{ 
+public:
+    explicit NoSuchClassException(const std::string&type_name);
+    virtual ~NoSuchClassException()throw();
+    virtual const char*what()const throw();
+protected: 
+    std::string message;
+};
+
 } // end namespace cppjson
 
--- src/utils.hpp
+++ src/utils.hpp
@@ -2,5 +2,24 @@
 
 namespace cppjson {
     
+struct EmptyClass{}; //在Reflectanle::get_method与Reflectable::classmethod_wrapper中有使用，用于类型转换的"中介"
+
+
+// ========================== has_config_member_function
+template<typename T,typename Enable = void >
+struct has_config_member_function_s : public std::false_type {};
+
+template<typename T>
+struct has_config_member_function_s< T, decltype(std::declval<T>().get_config()) > : public std::true_type{};
+
+template<typename T>
+constexpr bool has_config_member_function(){
+    return has_config_member_function_s<T>::value;
+}
+// ========================== has_config_member_function
+
+
+
+
 } // end namespace cppjson
 
